d0d62c1bc0eeeff75610c915c6dcde84e0022ce3
doomyster <doomyster@gmail.com>
2013-03-08 09:07:13 +0100

    Cosmetic: sanitization of indentation

--- mapcubus.js
+++ mapcubus.js
@@ -1,83 +1,82 @@
 var elementsArray = new Array();
 
 function convertIdToXy(id) {
-  var tab = id.split('-');
-  return new Array(parseInt(tab[1]), parseInt(tab[2]));
+    var tab = id.split('-');
+    return new Array(parseInt(tab[1]), parseInt(tab[2]));
 }
 
 function highlightArea(xy, lh) {
-  var distX = xy[0]+lh[0];
-  var distY = xy[1]+lh[1];
-  for(var i = xy[0]; i < distX; i++) {
-    for(var j = xy[1]; j < distY; j++) {
-      $('#c-'+i+'-'+j).addClass('highlight-yellow');
+    var distX = xy[0]+lh[0];
+    var distY = xy[1]+lh[1];
+    for(var i = xy[0]; i < distX; i++) {
+        for(var j = xy[1]; j < distY; j++) {
+            $('#c-'+i+'-'+j).addClass('highlight-yellow');
+        }
     }
-  }
 }
 
 function allElementsMove(xdir, ydir) {
-	$('.level-element').each(function() {
-		var xy_coords = convertIdToXy($(this).parent().attr("id"));
-		var x = xy_coords[0];
-		var y = xy_coords[1];
-		x += xdir;
-		y += ydir;
+    $('.level-element').each(function() {
+        var xy_coords = convertIdToXy($(this).parent().attr("id"));
+        var x = xy_coords[0];
+        var y = xy_coords[1];
+        x += xdir;
+        y += ydir;
 
-		// Should also test for overflow
-		if (x < 0 || y < 0) { 
-			$(this).remove();
-		}
-		else {
-			var newid = "c-" + x + "-" + y;
-			document.getElementById(newid).appendChild(this);
-		}
-	});
+        // Should also test for overflow
+        if (x < 0 || y < 0) { 
+            $(this).remove();
+        }
+        else {
+            var newid = "c-" + x + "-" + y;
+            document.getElementById(newid).appendChild(this);
+        }
+    });
 }
 
 
 function commitDistant(levelName) {
-	commitLocal();
-	$.post('server.php','level_name='+levelName+'&'+'level_content='+JSON.stringify(elementsArray), function(data) {
-		window.alert("level saved successfully !");
-	});	
+    commitLocal();
+    $.post('server.php','level_name='+levelName+'&'+'level_content='+JSON.stringify(elementsArray), function(data) {
+           window.alert("level saved successfully !");
+    });	
 }
 
 function revertDistant(levelName) {
-	$.get('server.php?level_name='+levelName, function(data) {
-		elementsArray = data;
-		revertLocal();
-	});	
+    $.get('server.php?level_name='+levelName, function(data) {
+        elementsArray = data;
+        revertLocal();
+    });	
 }
 
 function commitLocal() {
-	elementsArray = new Array(); // Of course, we need to erase all before filling it again !
-	$('.level-element').each(function() {
-	      var element = {
-		type: $(this).attr("data-type-template"),
-		item: $(this).attr("data-item-template"),
-		coordsId: $(this).parent().attr("id")
-	      }
-	      elementsArray.push(element);
-	});
+    elementsArray = new Array(); // Of course, we need to erase all before filling it again !
+    $('.level-element').each(function() {
+        var element = {
+            type: $(this).attr("data-type-template"),
+            item: $(this).attr("data-item-template"),
+            coordsId: $(this).parent().attr("id")
+        }
+        elementsArray.push(element);
+    });
 }
 
 function revertLocal() {
-	clearAllLevelElements();
-	for (var i = 0; i < elementsArray.length; i++) {
-	  currentItem = elementsArray[i];
-     var imgSrc = getImgSrc(currentItem.type, currentItem.item);
-	  var img = createLevelElement(currentItem.type, currentItem.item, imgSrc);
-	  $('#'+currentItem.coordsId).append(img);
-	  img.draggable(
-	  {
-		revert: false,
-		helper: "original"
-      });   
-	}
+    clearAllLevelElements();
+    for (var i = 0; i < elementsArray.length; i++) {
+        currentItem = elementsArray[i];
+        var imgSrc = getImgSrc(currentItem.type, currentItem.item);
+        var img = createLevelElement(currentItem.type, currentItem.item, imgSrc);
+        $('#'+currentItem.coordsId).append(img);
+        img.draggable({
+            revert: false,
+            helper: "original"
+        });   
+    }
 }
 
 function clearAllLevelElements() {
-	$('.level-element').remove();
+    $('.level-element').remove();
 }
 
 function getImgSrc(typeTpl, itemTpl) {
@@ -85,128 +84,123 @@ function getImgSrc(typeTpl, itemTpl) {
 }
 
 function createLevelElement(typeTpl, itemTpl, imgSrc) {
-	var img = $('<img class="level-element draggable-element" data-type-template="'+typeTpl+'" data-item-template="'+itemTpl+'" data-source="'+imgSrc.replace('tiles','icons')+'" src="'+imgSrc+'" />');
-	return img;
+    var img = $('<img class="level-element draggable-element" data-type-template="'+typeTpl+'" data-item-template="'+itemTpl+'" data-source="'+imgSrc.replace('tiles','icons')+'" src="'+imgSrc+'" />');
+    return img;
 }
 
 function createSaveForm() {
-	$("#save_form").dialog({
-	  autoOpen: false,
-	  height: 300,
-	  width: 350,
-	  modal: true,
-	  buttons: {
-	    "Save Level": function() {
-		commitDistant($('#level_name_save').val());
-		$( this ).dialog( "close" );
-	    },
-	    Cancel: function() {
-		$( this ).dialog( "close" );
-	    }
-	  },
-	  close: function() {
-	  }
-	});
-	
-	$("#commit-distant").click(function() {
-	  $("#save_form").dialog("open");
-	});
+    $("#save_form").dialog({
+        autoOpen: false,
+        height: 300,
+        width: 350,
+        modal: true,
+        buttons: {
+            "Save Level": function() {
+                commitDistant($('#level_name_save').val());
+                $( this ).dialog( "close" );
+            },
+            Cancel: function() {
+                $( this ).dialog( "close" );
+            }
+        },
+        close: function() {}
+    });
+
+    $("#commit-distant").click(function() {
+        $("#save_form").dialog("open");
+    });
 }
 
 function createLoadForm() {
-	$("#load_form").dialog({
-	  autoOpen: false,
-	  height: 300,
-	  width: 350,
-	  modal: true,
-	  buttons: {
-	    "Load Level": function() {
-		revertDistant($('#level_name_load').val());
-		$(this).dialog( "close" );
-	    },
-	    Cancel: function() {
-		$(this).dialog( "close" );
-	    }
-	  },
-	  close: function() {
-	  }
-	});
-	
-	$("#revert-distant").click(function() {
-	  $("#load_form").dialog("open");
-	});	
+    $("#load_form").dialog({
+        autoOpen: false,
+        height: 300,
+        width: 350,
+        modal: true,
+        buttons: {
+            "Load Level": function() {
+                revertDistant($('#level_name_load').val());
+                $(this).dialog( "close" );
+            },
+            Cancel: function() {
+                $(this).dialog( "close" );
+            }
+        },
+        close: function() {}
+    });
+
+    $("#revert-distant").click(function() {
+        $("#load_form").dialog("open");
+    });	
 }
 
 $(document).ready(function() {
-  $(".draggable").draggable(
-  {
-	  revert: false, 
-	  helper: "clone",
-	  scroll: false,
-	  zIndex: 3200
-  });
-	  	
-  $(".drop-target").droppable({
-     drop: function( event, ui ) {
-	  var imgSrc = $(ui.draggable).attr("data-source");
-	  var itemTpl = $(ui.draggable).attr("data-item-template");
-	  var typeTpl = $(ui.draggable).attr("data-type-template");
-	  var img = createLevelElement(typeTpl, itemTpl, imgSrc);
-	  $(this).append(img);
-	  img.draggable(
-	  {
-		revert: false,
-		helper: "original"
-          });
-	   $(".drop-target").removeClass('highlight-yellow');
-      },
-      over: function(event, ui) {
-	  $(".drop-target").removeClass('highlight-yellow');
-	  // Récupération des coordonnées de la cellule
-	  var xy = convertIdToXy($(this).attr("id"));
-	  //TODO: insérer la place en case dans le xml dans les attributs data-x et data-y, la récupérer ici pour alimenter la fonction highlightArea
-     // Attention aux rotations
-	  var lh = new Array(parseInt($(ui.draggable).attr("data-x")),parseInt($(ui.draggable).attr("data-y")));
-	  highlightArea(xy,lh);
-	  $(this).addClass('highlight-yellow');
-      },
-      out: function(event, ui) {
-	  $(".drop-target").removeClass('highlight-yellow');
-      }
-  });
+    $(".draggable").draggable({
+        revert: false, 
+        helper: "clone",
+        scroll: false,
+        zIndex: 3200
+    });
 
-  $("#accordeon").accordion({
-  	collapsible: true
-  });
-  
-  createSaveForm();
-  createLoadForm();
-  /*$("#commit-distant").click(function() {
-    commitDistant();
-  });*/
-  
-  /*$("#revert-distant").click(function() {
-    revertDistant();
-  });*/
-    
-  $("#commit-local").click(function() {
-    commitLocal();
-  });
-  $("#revert-local").click(function() {
-    revertLocal();
-  });
+    $(".drop-target").droppable({
+        drop: function( event, ui ) {
+            var imgSrc = $(ui.draggable).attr("data-source");
+            var itemTpl = $(ui.draggable).attr("data-item-template");
+            var typeTpl = $(ui.draggable).attr("data-type-template");
+            var img = createLevelElement(typeTpl, itemTpl, imgSrc);
+            $(this).append(img);
+            img.draggable({
+                revert: false,
+                helper: "original"
+            });
+            $(".drop-target").removeClass('highlight-yellow');
+        },
+        over: function(event, ui) {
+            $(".drop-target").removeClass('highlight-yellow');
+            // Récupération des coordonnées de la cellule
+            var xy = convertIdToXy($(this).attr("id"));
+            //TODO: insérer la place en case dans le xml dans les attributs data-x et data-y, la récupérer ici pour alimenter la fonction highlightArea
+            // Attention aux rotations
+            var lh = new Array(parseInt($(ui.draggable).attr("data-x")),parseInt($(ui.draggable).attr("data-y")));
+            highlightArea(xy,lh);
+            $(this).addClass('highlight-yellow');
+        },
+        out: function(event, ui) {
+            $(".drop-target").removeClass('highlight-yellow');
+        }
+    });
 
-  $("#all-move-left").click(function() {
-    allElementsMove(-1, 0);
-  });
-  $("#all-move-up").click(function() {
-    allElementsMove(0, -1);
-  });
-  $("#all-move-down").click(function() {
-    allElementsMove(0, 1);
-  });
-  $("#all-move-right").click(function() {
-    allElementsMove(1, 0);
-  });
+    $("#accordeon").accordion({
+        collapsible: true
+    });
 
+    createSaveForm();
+    createLoadForm();
+    /*$("#commit-distant").click(function() {
+        commitDistant();
+    });*/
+
+    /*$("#revert-distant").click(function() {
+        revertDistant();
+    });*/
+
+    $("#commit-local").click(function() {
+        commitLocal();
+    });
+    $("#revert-local").click(function() {
+        revertLocal();
+    });
+
+    $("#all-move-left").click(function() {
+        allElementsMove(-1, 0);
+    });
+    $("#all-move-up").click(function() {
+        allElementsMove(0, -1);
+    });
+    $("#all-move-down").click(function() {
+        allElementsMove(0, 1);
+    });
+    $("#all-move-right").click(function() {
+        allElementsMove(1, 0);
+    });
 });
--- mapcubus.php
+++ mapcubus.php
@@ -13,155 +13,149 @@ function genererTitreSection($chemin) {
   return $parties[count($parties) - 1];
 }
 
-class Category
-{
-	public $name = '';
-	public $template = '';
-	public $layer = '';
-	public $items = array();
+class Category {
+    public $name = '';
+    public $template = '';
+    public $layer = '';
+    public $items = array();
 
-	function getFiles() {
-		$r = array();
-		foreach($this->items as $i) {
-			foreach($i->getFiles() as $ii) {
-				$r[] = $this->template . '/' . $ii;
-			}
-			
-		}
-		return $r;
-	}
+    function getFiles() {
+        $r = array();
+        foreach($this->items as $i) {
+            foreach($i->getFiles() as $ii) {
+                $r[] = $this->template . '/' . $ii;
+            }
+
+        }
+        return $r;
+    }
 }
 
-class Item
-{
-	public $name = '';
-	public $template = '';
-	public $rotation = '';
-	public $types = array();
+class Item {
+    public $name = '';
+    public $template = '';
+    public $rotation = '';
+    public $types = array();
 
-	function getFiles() {
-		$rr = array();
+    function getFiles() {
+        $rr = array();
 
-		for ($r=0; $r <= $this->rotation; $r++)
-		{
-			if (count($this->types) == 0)
-				$rr[] = $this->template . "_$r.png";
-			else {
-				foreach($this->types as $t) {
-					$rr[] = $this->template . $t->template . "_$r.png";
-				}
-			}
-		}
-		return $rr;
-	}
+        for ($r=0; $r <= $this->rotation; $r++) {
+            if (count($this->types) == 0)
+                $rr[] = $this->template . "_$r.png";
+            else {
+                foreach($this->types as $t) {
+                    $rr[] = $this->template . $t->template . "_$r.png";
+                }
+            }
+        }
+        return $rr;
+    }
 }
 
 class Type
 {
-	public $name = '';
-	public $template = '';
+    public $name = '';
+    public $template = '';
 }
 
 function ScanXmlData($data) {
-  $xmlstr = file_get_contents($data);
-  $a = new SimpleXMLElement($xmlstr);
+    $xmlstr = file_get_contents($data);
+    $a = new SimpleXMLElement($xmlstr);
 
-  $categories = array();
-  $counter = 0;
-  foreach ($a->children() as $c) {
-  	if ($c->getName() == "category") {
-		$cat = new Category();
+    $categories = array();
+    $counter = 0;
+    foreach ($a->children() as $c) {
+        if ($c->getName() == "category") {
+            $cat = new Category();
 
-		foreach ($c->attributes() as $k=>$v) {
-			if ($k == 'name')
-				$cat->name = "$v";
-			else if ($k == 'template')
-				$cat->template = "$v";
-			else if ($k == 'layer')
-				$cat->layer = "$v";
-		}
-		if ($cat->name == '' || $cat->template == '' || $cat->layer == '')
-			die("Incomplete category entry; name='$cat->name', template='$cat->template', layer='$cat->layer'.");
+            foreach ($c->attributes() as $k=>$v) {
+                if ($k == 'name')
+                    $cat->name = "$v";
+                else if ($k == 'template')
+                    $cat->template = "$v";
+                else if ($k == 'layer')
+                    $cat->layer = "$v";
+            }
+            if ($cat->name == '' || $cat->template == '' || $cat->layer == '')
+                die("Incomplete category entry; name='$cat->name', template='$cat->template', layer='$cat->layer'.");
 
 
-		foreach ($c->children() as $i) {
-			if ($i->getName() == "item") {
-				$item = new Item();
+            foreach ($c->children() as $i) {
+                if ($i->getName() == "item") {
+                    $item = new Item();
 
-				foreach ($i->attributes() as $k=>$v) {
-					if ($k == 'name')
-						$item->name = "$v";
-					else if ($k == 'template')
-						$item->template = "$v";
-					else if ($k == 'rotation')
-						$item->rotation = "$v";
-				}
-				if ($item->name == '' || $item->template == '')
-					die("Incomplete item entry; name='$item->name', template='$item->template', rotation='$item->rotation'.");
-				if ($item->rotation == '')
-					$item->rotation = '0';
+                    foreach ($i->attributes() as $k=>$v) {
+                        if ($k == 'name')
+                            $item->name = "$v";
+                        else if ($k == 'template')
+                            $item->template = "$v";
+                        else if ($k == 'rotation')
+                            $item->rotation = "$v";
+                    }
+                    if ($item->name == '' || $item->template == '')
+                        die("Incomplete item entry; name='$item->name', template='$item->template', rotation='$item->rotation'.");
+                    if ($item->rotation == '')
+                        $item->rotation = '0';
 
-				foreach ($i->children() as $t) {
-					if ($t->getName() == "type") {
-						$type = new Type();
+                    foreach ($i->children() as $t) {
+                        if ($t->getName() == "type") {
+                            $type = new Type();
 
-						foreach($t->attributes() as $k=>$v) {
-							if ($k == 'name')
-								$type->name = "$v";
-							else if ($k == 'template')
-								$type->template = "$v";
-						}
-						$item->types[] = $type;
-					}
-				} // End of: iterate item's children
-				$cat->items[] = $item;
-			} // End of: element is 'item'
-		} // End of: iterate catecory's children
-		$categories[] = $cat;
-	} // End of: element is 'category'
-  } // End of: iterate root's children
-  $pieces = '';
-  foreach ($categories as $cat) {
-	$pieces .= '<h3><a href="#">' . $cat->name . '</a></h3>';
-	$pieces .= '<div class="categorie_menu"><p>';
+                            foreach($t->attributes() as $k=>$v) {
+                                if ($k == 'name')
+                                    $type->name = "$v";
+                                else if ($k == 'template')
+                                    $type->template = "$v";
+                            }
+                            $item->types[] = $type;
+                        }
+                    } // End of: iterate item's children
+                    $cat->items[] = $item;
+                } // End of: element is 'item'
+            } // End of: iterate catecory's children
+            $categories[] = $cat;
+        } // End of: element is 'category'
+    } // End of: iterate root's children
+    $pieces = '';
+    foreach ($categories as $cat) {
+        $pieces .= '<h3><a href="#">' . $cat->name . '</a></h3>';
+        $pieces .= '<div class="categorie_menu"><p>';
 
-	foreach($cat->items as $i) {
-		$imgs = $i->getFiles();
-		$counter = 0;
-		foreach ($imgs as $im) {
-			$size = getimagesize('http://localhost/mapcubus/tiles/'.$cat->template.'/'.$im);
-			$pieces .= '<span class="feuille_menu"><img class="illustration_menu draggable" data-item-template="'.$cat->template.'" data-type-template="'.$im.'" data-x="'.($size[0]/64).'" data-y="'.($size[1]/64).'"  data-source="http://localhost/mapcubus/tiles/' . $cat->template . '/' . $im . '" src="http://localhost/mapcubus/icons/' . $cat->template . '/' . $im . '" alt="' . $i->name . '"/></span>';
-			if ($counter == 1) {
-				$counter = -1;
-				$pieces .= '<br>';
-			}
-			$counter++;
-		}
-		$pieces .= '<hr />';
-	}
+        foreach($cat->items as $i) {
+            $imgs = $i->getFiles();
+            $counter = 0;
+            foreach ($imgs as $im) {
+                $size = getimagesize('http://localhost/mapcubus/tiles/'.$cat->template.'/'.$im);
+                $pieces .= '<span class="feuille_menu"><img class="illustration_menu draggable" data-item-template="'.$cat->template.'" data-type-template="'.$im.'" data-x="'.($size[0]/64).'" data-y="'.($size[1]/64).'"  data-source="http://localhost/mapcubus/tiles/' . $cat->template . '/' . $im . '" src="http://localhost/mapcubus/icons/' . $cat->template . '/' . $im . '" alt="' . $i->name . '"/></span>';
+                if ($counter == 1) {
+                    $counter = -1;
+                    $pieces .= '<br>';
+                }
+                $counter++;
+            }
+            $pieces .= '<hr />';
+        }
 
-	$pieces .= '</p></div>';
-  }
-  return $pieces;
+        $pieces .= '</p></div>';
+    }
+    return $pieces;
 }
 
 $liste = ScanXmlData("./data.xml");
 
-function genererGrille()
-{
-	$grille = "";
-	for ($i=0; $i < 64; $i++)
-	{
-		$grille .= '<div class="ligne">';
-		for ($j=0; $j < 64; $j++)
-		{
-			$grille .= '<div id="c-'.$j.'-'.$i.'"';
-			$grille .= ' class="cellule drop-target" >';
-			$grille .= "</div>";
-		}
-		$grille .= "</div>";
-	}
-	return $grille;
+function genererGrille() {
+    $grille = "";
+    for ($i=0; $i < 64; $i++) {
+        $grille .= '<div class="ligne">';
+        for ($j=0; $j < 64; $j++) {
+            $grille .= '<div id="c-'.$j.'-'.$i.'"';
+            $grille .= ' class="cellule drop-target" >';
+            $grille .= "</div>";
+        }
+        $grille .= "</div>";
+    }
+    return $grille;
 }
 
 $contenu = genererGrille();
@@ -172,4 +166,5 @@ $sortie = ob_get_contents();
 
 @ob_end_clean();
 echo $sortie;
+
 ?>
